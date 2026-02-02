c49bdf3169938bcacb3836c72b8eca0651f804a0
AVG <glazyrinav1986@yandex.ru>
2026-02-02 17:49:09 +0300

    feat: добавление класса Header

---
+++ src/components/view/header/header.ts
@@ -0,0 +1,26 @@
+import {ensureElement} from "../../../utils/utils.ts";
+import {Component} from "../../base/Component.ts";
+import {IEvents} from "../../base/Events.ts";
+
+interface IHeader {
+    counter: number;
+}
+
+export class Header extends Component<IHeader> {
+    protected basketButton: HTMLButtonElement;
+    protected counterElement: HTMLElement;
+
+    constructor(protected events: IEvents, container: HTMLElement) {
+        super(container)
+        this.basketButton = ensureElement<HTMLButtonElement>('header__basket-counter', this.container);
+        this.counterElement = ensureElement<HTMLElement>("header__basket", this.container);
+
+        this.basketButton.addEventListener('click', () => {
+            this.events.emit('basket:open');
+        });
+    }
+
+    public set counter(value: number) {
+        this.counterElement.textContent = String(value);
+    }
+}
