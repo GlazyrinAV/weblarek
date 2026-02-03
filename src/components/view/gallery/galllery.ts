1aca6d9c38248f8cf45f964d36ad0da01c8e7209
AVG <glazyrinav1986@yandex.ru>
2026-02-03 08:35:54 +0300

    feat: создание структуры gallery

---
+++ src/components/view/gallery/galllery.ts
@@ -0,0 +1,26 @@
+import {Component} from "../../base/Component.ts";
+import {ensureElement} from "../../../utils/utils.ts";
+
+interface IGalleryData {
+    catalog: HTMLElement[];
+}
+
+export class Gallery extends Component<IGalleryData> {
+    protected catalogElement: HTMLElement;
+
+    constructor(container: HTMLElement) {
+        super(container);
+
+        this.catalogElement = ensureElement<HTMLElement>('gallery', container);
+    }
+
+    public setCatalog(items: HTMLElement[]) {
+        let list: HTMLUListElement = document.createElement('ul');
+        items.forEach(item => {
+            let listItem = document.createElement('li');
+            listItem.classList.add('gallery__item')
+            listItem.append(item);
+            list.append(listItem);
+        })
+    }
+}
--- src/components/view/header/header.ts
+++ src/components/view/header/header.ts
@@ -2,18 +2,18 @@ import {ensureElement} from "../../../utils/utils.ts";
 import {Component} from "../../base/Component.ts";
 import {IEvents} from "../../base/Events.ts";
 
-interface IHeader {
+interface IHeaderData {
     counter: number;
 }
 
-export class Header extends Component<IHeader> {
+export class Header extends Component<IHeaderData> {
     protected basketButton: HTMLButtonElement;
     protected counterElement: HTMLElement;
 
     constructor(protected events: IEvents, container: HTMLElement) {
         super(container)
-        this.basketButton = ensureElement<HTMLButtonElement>('header__basket-counter', this.container);
-        this.counterElement = ensureElement<HTMLElement>("header__basket", this.container);
+        this.basketButton = ensureElement<HTMLButtonElement>('header__basket', this.container);
+        this.counterElement = ensureElement<HTMLElement>('header__basket-counter', this.container);
 
         this.basketButton.addEventListener('click', () => {
             this.events.emit('basket:open');
