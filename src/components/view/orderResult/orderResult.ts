7055e11456dd8d900700aa320f8c016601e4946f
AVG <glazyrinav1986@yandex.ru>
2026-02-03 09:24:05 +0300

    feat: создание структуры orderResult

---
+++ src/components/view/orderResult/orderResult.ts
@@ -0,0 +1,27 @@
+import {Component} from "../../base/Component.ts";
+import {IEvents} from "../../base/Events.ts";
+import {ensureElement} from "../../../utils/utils.ts";
+
+interface IOrderResult {
+    total: number;
+}
+
+export class OrderResult extends Component<IOrderResult> {
+    protected descriptionElement: HTMLElement;
+    protected closeButton: HTMLButtonElement;
+
+    constructor(protected events: IEvents, container: HTMLElement) {
+        super(container);
+
+        this.descriptionElement = ensureElement<HTMLElement>('order-success__description', this.container);
+        this.closeButton = ensureElement<HTMLButtonElement>('order-success__close', this.container);
+
+        this.closeButton.addEventListener('click', () => {
+            events.emit('result:close');
+        })
+    }
+
+    public set description(total: number) {
+        this.descriptionElement.textContent = `Списано ${total} синапсов`;
+    }
+}
